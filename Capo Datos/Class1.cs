765e8307c7f7405c4d0146d5e82014c7c01029ee
adria <adria@DESKTOP-LQEAI55>
2021-11-11 23:22:33 -0400

    Initial Commit

---
+++ Capo Datos/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Capo_Datos
+{
+    public class Class1
+    {
+    }
+}
---
+++ IB Banco XY/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IB_Banco_XY.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
