e3b5b94d773fd8ab88adcea1a4549b10ed55a338
ECHAOD <adrianeea@hotmail.com>
2021-11-23 00:32:26 -0400

    Agregando nuevas Capas

---
+++ Contratos/IRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Contratos
+{
+    public interface IRepository<T, E>
+    {
+
+        public Task Create(T entity);
+
+        public Task Delete(T entity);
+
+        public Task<IQueryable<T>> FindByCondition(Expression<Func<T, bool>> expresion);
+
+        public Task<T> FindById(E id);
+
+        public Task<IQueryable<T>> GetAll();
+
+        public Task Update(T entity);
+    }
+}
